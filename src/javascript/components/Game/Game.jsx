b3bb7d5bbc58a505ae90a48af4bef8c9d61e01f7
Aneerudh P <Aneerudh.P@mrcooper.com>
2018-07-15 17:04:19 +0530

    Add: color to the players turn box based on player colour code

--- src/javascript/components/Game/Game.jsx
+++ src/javascript/components/Game/Game.jsx
@@ -4,11 +4,12 @@ import Board from '../Board/Board'
 
 const Game = ({ currentPlayer, stillSamePlayer }) => {
     const player = currentPlayer === 0 ? 'Blue' : 'Red'
-    const status = 'Player turn : ' + player;
+    const status = player + '\'s turn!' ;
+    const statusColour = 'status-' + player.toLowerCase();
     const samePlayerMessage = 'Broooooo, this is not your square! Play somewhere else! ';
     return (
         <div>
-            <div className="status"> {status} </div>
+            <div className={statusColour}> {status} </div>
             {stillSamePlayer ? <div className='same-player-message' > {samePlayerMessage} </div> : null}
             <Board classname={player} />
         </div>
